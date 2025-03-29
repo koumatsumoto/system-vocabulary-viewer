448e01e6eca97e57fc8c6dac6bffb2d86981c12e
kou <kou.matsumoto.jp@gmail.com>
2025-03-30 01:12:37 +0900

    feat: improve WordListItem style

--- src/app/word/WordListItem.tsx
+++ src/app/word/WordListItem.tsx
@@ -10,23 +10,37 @@ export default function WordListItem({ word }: { word: Word }) {
       <h2 className="text-2xl text-gray-800 font-bold mb-2">{word.name}</h2>
       {word.alias && <h3 className="text-lg text-gray-600 mb-4">({word.alias.join(", ")})</h3>}
 
-      <div className="space-y-12">
-        {word.definitions.map((def, index) => (
-          <div key={index} className="text-gray-800">
-            <p className="text-base">{def.text}</p>
-            {def.reference && <p className="text-sm text-gray-500 mt-2">Reference: {def.reference}</p>}
-          </div>
-        ))}
+      <div className="space-y-4">
+        <div className="my-4">
+          {word.definitions.map((def, index) => (
+            <div key={index} className="my-2">
+              <p className="text-base text-gray-800">
+                {index + 1}. {def.text}
+              </p>
+              {def.reference && <p className="text-sm text-gray-500 mt-2">Reference: {def.reference}</p>}
+            </div>
+          ))}
+        </div>
 
-        {word.example && (
-          <div className="mt-4 p-4 bg-gray-50 rounded-lg">
-            <p className="text-sm text-gray-700">Example: {word.example}</p>
-          </div>
-        )}
+        <div className="mt-8">
+          {word.example && (
+            <div className="py-2 bg-gray-50 rounded-lg">
+              <p className="text-sm text-gray-700">Example: {word.example}</p>
+            </div>
+          )}
 
-        {word.confer && <p className="text-sm text-gray-600 mt-4">c.f. {word.confer.join(", ")}</p>}
+          {word.confer && (
+            <div className="py-2 bg-gray-50 rounded-lg">
+              <p className="text-sm text-gray-700">c.f. {word.confer.join(", ")}</p>
+            </div>
+          )}
 
-        {word.note && <p className="text-sm text-gray-600 italic mt-4">Note: {word.note}</p>}
+          {word.note && (
+            <div className="py-2 bg-gray-50 rounded-lg">
+              <p className="text-sm text-gray-700 italic">Note: {word.note}</p>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
